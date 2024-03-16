c2100b84f06fbf910429a8bad9a871e62199d876
Pedro-Prado-Dev <pedroppaiva1@hotmail.com>
2024-03-16 13:28:27 -0300

    :sparkles: feat: add tasks to ToDoList

---
+++ src/TodoList.ts
@@ -0,0 +1,16 @@
+type Task = {
+    title: string,
+    description: string,
+    targetDate: string,
+    type?: string,
+    priority?: string,
+    subTask?: Task [],
+}
+
+export class ToDoList{
+    private tasks: Task[] = []
+
+    add(task: Task){
+        this.tasks.push(task)
+    }
+}
